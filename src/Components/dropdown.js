1aaf333eb0411e2c39dc280fdc001db69821c66a
Kevin Liu <kevinliu3931@gmail.com>
2022-01-03 01:48:14 -0500

    Dropdown
    Replaced the dropdown button with an 'X' when it's opened

--- src/Components/dropdown.js
+++ src/Components/dropdown.js
@@ -1,51 +1,71 @@
 import Classes from "./dropdown.module.css";
-import {HashLink} from "react-router-hash-link";
-import {useState} from "react";
-import onClickOutside from 'react-onclickoutside';
+import { HashLink } from "react-router-hash-link";
+import { useState } from "react";
+import onClickOutside from "react-onclickoutside";
 
 function DropDown() {
-    const [DropDownIsOpen, setDropDownIsOpen] = useState(false);
-    DropDown.handleClickOutside = () => setDropDownIsOpen(false);
+  const [DropDownIsOpen, setDropDownIsOpen] = useState(false);
+  DropDown.handleClickOutside = () => setDropDownIsOpen(false);
 
+  function open() {
+    setDropDownIsOpen(true);
+  }
+  function Close() {
+    setDropDownIsOpen(false);
+  }
+  let menu = "";
+  if (setDropDownIsOpen) {
+  }
 
-    function open() {
-        setDropDownIsOpen(true);
-    }
-    function Close() {
-        setDropDownIsOpen(false);
-    }
-
-
-    return (
-        <div>
-            <div className={Classes.container}>
-                <button type="button" onClick={open} >
-                ☰
-                </button>
-                {DropDownIsOpen ? (
-                <div className={Classes.drop}>
-                    <ul>
-                        <li>
-                            <HashLink smooth to="#about" onClick={Close}>About</HashLink>
-                        </li>
-                        <li>
-                            <HashLink smooth to="#portfolio" onClick={Close}>Portfolio</HashLink>
-                        </li>
-                        <li>
-                            <HashLink smooth to="#contact" onClick={Close}>Contact me</HashLink>
-                        </li>
-                    </ul>
-                    <button type="button" className={Classes.cancelButton} onClick={Close}>
-                        X
-                    </button>
-                </div>
-                ) : null} 
-            </div>
-        </div>
+  let a = "";
+  if (!DropDownIsOpen) {
+    a = (
+      <button type="button" onClick={open}>
+        ☰
+      </button>
     );
+  }
+  let b = "";
+  if (DropDownIsOpen) {
+    b = (
+      <button type="button" onClick={Close}>
+        X
+      </button>
+    );
+  }
+
+  return (
+    <div>
+      <div className={Classes.container}>
+        {a}
+        {b}
+        {DropDownIsOpen ? (
+          <div className={Classes.drop}>
+            <ul>
+              <li>
+                <HashLink smooth to="#about" onClick={Close}>
+                  About
+                </HashLink>
+              </li>
+              <li>
+                <HashLink smooth to="#portfolio" onClick={Close}>
+                  Portfolio
+                </HashLink>
+              </li>
+              <li>
+                <HashLink smooth to="#contact" onClick={Close}>
+                  Contact me
+                </HashLink>
+              </li>
+            </ul>
+          </div>
+        ) : null}
+      </div>
+    </div>
+  );
 }
 
 const clickOutsideConfig = {
-    handleClickOutside: () => DropDown.handleClickOutside,
+  handleClickOutside: () => DropDown.handleClickOutside,
 };
-export default onClickOutside(DropDown,clickOutsideConfig);
+export default onClickOutside(DropDown, clickOutsideConfig);

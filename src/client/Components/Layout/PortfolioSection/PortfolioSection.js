24716481e7e79ffd3cdde508fd36c85ccabcdf26
Kevin Liu <kevinliu3931@gmail.com>
2022-06-04 01:22:06 -0400

    Setting up github API

--- src/client/Components/Layout/PortfolioSection/PortfolioSection.js
+++ src/client/Components/Layout/PortfolioSection/PortfolioSection.js
@@ -1,11 +1,47 @@
 import Classes from "./PortfolioSection.module.css";
-import React from "react";
+import React, {useState} from "react";
 
 function PortfolioSection() {
+  const [test, setTest] = useState([]);
+  
+
+  function getUserRepo(username) {
+    let test = [];
+    const req = new XMLHttpRequest();
+    const url = `https://api.github.com/users/${username}/starred`
+    req.open('GET', url, true); //Opens our connection to the github server
+    
+    
+
+      req.onload = function () {
+        const data = JSON.parse(this.response);
+        for (let i in data) {
+          test.push(data[i]);
+        }
+        setTest(data);
+      };
+      req.send();
+  }
+
+  
     return(
   <section className={Classes.portfolioSection}  id="portfolio">
     <header>My Projects</header>
-    <paragraph>My current projects</paragraph>
+    <div id = {Classes["test"]}>
+      {getUserRepo('kliu6151')}
+      {test.map((repo) => (
+        <>
+        <div className={Classes.repoGrid}>
+          {repo.name}
+          <br />
+          {repo.description}
+          <br />
+          <a href = {repo.html_url}> hi</a>
+        </div>
+        </>
+      ))}
+    </div>
+    {/* <paragraph>My current projects</paragraph> */}
   </section>
     );
 }

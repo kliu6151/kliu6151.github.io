945c6a7af2d142dd941be4a5c86abc52fe7b01d5
Kevin Liu <kevinliu3931@gmail.com>
2022-06-06 00:04:10 -0400

    implementing flexbox into portfolio section

--- src/client/Components/Layout/PortfolioSection/PortfolioSection.js
+++ src/client/Components/Layout/PortfolioSection/PortfolioSection.js
@@ -2,42 +2,62 @@ import Classes from "./PortfolioSection.module.css";
 import React, {useState} from "react";
 
 function PortfolioSection() {
-  const [test, setTest] = useState([]);
+  const [wantedRepo, setWantedRepo] = useState([]);
   
 
   function getUserRepo(username) {
-    let test = [];
+    let wantedRepoTemp = [];
     const req = new XMLHttpRequest();
     const url = `https://api.github.com/users/${username}/starred`
     req.open('GET', url, true); //Opens our connection to the github server
-    
+    let manualWork = ['kliu6151.github.io','TDS'];
     
 
       req.onload = function () {
         const data = JSON.parse(this.response);
-        for (let i in data) {
-          test.push(data[i]);
+        for(let i in manualWork) {
+          // console.log(i);
+          var pos = data.map(function(x) { return x.name; }).indexOf(manualWork[i]);
+          wantedRepoTemp.push(data[pos]);
         }
-        setTest(data);
+        // console.log(wantedRepoTemp)
+        // for (let i in data) {
+        //   if(data[i].name === "kliu6151.github.io") {
+        //     test.push(data[i]);
+        //   }
+        // }
+        setWantedRepo(wantedRepoTemp);
       };
       req.send();
   }
-
   
     return(
   <section className={Classes.portfolioSection}  id="portfolio">
     <header>My Projects</header>
     <div id = {Classes["test"]}>
       {getUserRepo('kliu6151')}
-      {test.map((repo) => (
+      {wantedRepo.map((repo,index) => (
         <>
-        <div className={Classes.repoGrid}>
-          {repo.name}
-          <br />
-          {repo.description}
-          <br />
-          <a href = {repo.html_url}> hi</a>
+        {index}
+        <div className={Classes.flexContainer}>
+            <div className={Classes.repoGrid}>
+              {repo.name}
+              <br />
+              {repo.description}
+              <br />
+              <a href = {repo.html_url}>Access Here</a>
+            </div>
+            {index !== wantedRepo.length - 1 &&
+            <div className={Classes.repoGrid}>
+              {wantedRepo[++index].name}
+              <br />
+              {wantedRepo[index].description}
+              <br />
+              <a href = {wantedRepo[index].html_url}>Access Here</a>
+            </div>
+            }
         </div>
+        
         </>
       ))}
     </div>
